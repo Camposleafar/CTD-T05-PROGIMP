3efb8966a42496ecc4174b1ddf3586ba8ba7e9d2
Campos, Rafael <89111400+Camposleafar@users.noreply.github.com>
2021-11-11 14:50:47 -0300

    Add files via upload

---
+++ checkpoint.js
@@ -0,0 +1,73 @@
+
+var prato1 = 'pipoca';
+var segundos1 = 10;
+
+function microondas(prato, segundos){
+    let prato1 = "carne";
+    if (prato1 && segundos1-5)
+    console.log('tempo insuficiente');
+    return prato1;
+}
+let resultado = microondas(prato1);{
+
+}
+//////////////////////////////////////
+
+
+var prato2 = 'macarrao';
+var segundos2 = 8;
+
+function microondas(prato, segundos){
+    let prato2 = "macarrao";
+    if (prato2 && segundos2)
+    console.log('prato inesistente', NaN);
+    return prato2;
+}
+let resultado = microondas(prato2);{
+
+}
+
+//////////////////////////////////////
+
+var prato3 = 'carne';
+var segundos3 = 15;
+
+function microondas(prato, segundos){
+    let prato3 = "carne";
+    if (prato3 && segundos3*2)
+    console.log('comida queimou');
+    return prato3;
+}
+let resultado = microondas(prato3);{
+
+}
+
+/////////////////////////////////////
+
+var prato4 = 'feijao';
+var segundos4 = 12;
+
+function microondas(prato, segundos){
+    let prato4 = "feijao";
+    if (prato4 && segundos4*5)
+    console.log('kabum');
+    return prato4;
+}
+let resultado = microondas(prato4);{
+
+}
+
+////////////////////////////////////
+
+var prato5 = 'brigadeiro';
+var segundos5 = 8;
+
+function microondas(prato, segundos){
+    let prato5 = "brigadeiro";
+    if (prato3 && segundos5)
+    console.log('prato pronto, bom apetite');
+    return prato5;
+}
+let resultado = microondas(prato5);{
+
+}
